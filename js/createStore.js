7e46769324772c93ef8f61815668bf81e4d5b9da
Brian Ly <brian.kyang.ly@gmail.com>
2019-11-19 11:57:02 -0800

    finished lab

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,39 @@
-let state;
+const createStore = reducer => {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  const getState = () => {
+    return state;
+  };
+
+  return { dispatch, getState };
+};
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
+    case "INCREASE_COUNT":
       return { count: state.count + 1 };
+    case "@@INIT":
+      return { count: 0 };
 
     default:
       return state;
   }
-};
+}
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+let store = createStore(reducer);
+store.dispatch({ type: "@@INIT" });
+
+let button = document.getElementById("button");
+button.addEventListener("click", function() {
+  store.dispatch({ type: "INCREASE_COUNT" });
+});
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
-
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+  let container = document.getElementById("container");
+  container.textContent = store.getState().count;
+}
